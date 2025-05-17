a0277d31211e62bab60a656006fcf23a62048ff8
KazbekMusaev <145585208+KazbekMusaev@users.noreply.github.com>
2025-05-17 08:53:59 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by KazbekMusaev on 17.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
